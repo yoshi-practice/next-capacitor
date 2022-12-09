b04c823a979a178544f0f325e90f9e83efb906a5
yosh1 <yoppy1125yoppy@gmail.com>
2022-12-10 01:23:34 +0900

    Add some setting

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  trailingSlash: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    }
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
